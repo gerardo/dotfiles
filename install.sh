118d6ba43c1fdc4eb1abd035facef66a3f2fc643
Gerardo Curiel <gerardo@gerardo.cc>
2011-05-25 00:15:16 -0430

    Install.sh (yeah, .sh, not ruby, sorry to disappoint you)

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+       # It's not a directory and is identical
+       if [ -f "${target}" ] &&  cmp $source $target &> /dev/null ; then
+           echo "Identical: $target"
+       else
+          mv $target $target.bak 
+          echo "Linking $target"          
+          ln -sf ${source} ${target}
+       fi
+    else
+        echo "Linking $target"    
+        ln -sf ${source} ${target}
+    fi
+}
+
+echo "Linking dotfiles..."
+
+for i in _*
+  do
+    link_file $i
+  done
+
+cd _vim/
+
+vimfiles=( _vimrc _gvimrc )
+
+for i in ${vimfiles[@]}
+  do
+    link_file $i 
+  done
+
+cd ..
+
+echo "Git syncing"
+
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+
+echo "Building command-t"
+
+# setup command-t
+cd _vim/bundle/command-t
+rake make
+
+echo "Done!"
+
