1a2a429592b05ee5f4ed1ddca2d0275bc7fd8612
Gerardo Curiel <gerardo@gerar.do>
2026-04-30 12:27:52 +1000

    Lazyvim with TokyoNight

---
+++ _config.nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
